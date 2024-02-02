ac3e061a0ac2938da4dde824476b4c6b81d8ee86
Emil Olszewski <eemil.olszewski@gmail.com>
2024-02-02 11:51:41 +0100

    biblioteka statsmodels

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd 
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import statsmodels.api as sm
+
 from consts import * 
 
 
@@ -36,15 +39,22 @@ electricity_data = load_data('data/electricity_prices_day_ahead_hourly_all.csv')
 
 # Only take data from 01.01.2016 to 31.12.2017
 electricity_data = electricity_data[0:17543]
+monthly_data = electricity_data['fixing_i_volume'][0:SAMPLES_PER_MONTH]
+
+# plot_time_series(electricity_data, 'fixing_i_volume')
+plot_acf(monthly_data, lags=20, adjusted=True)
+plot_pacf(monthly_data, lags=20)
+result = sm.tsa.seasonal_decompose(monthly_data, period=SAMPLES_PER_DAY)
+result.plot()
+plt.show()
 
 
-plot_time_series(electricity_data, 'fixing_i_volume')
 
-# Dane dla jednego dnia 
-plot_time_series(electricity_data[0:SAMPLES_PER_DAY], 'fixing_i_volume', title='Dane dla jednego dnia')
-# Dane dla tygodnia 
-plot_time_series(electricity_data[0:SAMPLES_PER_WEEK], 'fixing_i_volume', title='Dane dla tygodnia')
-# Dane dla miesiąca
-plot_time_series(electricity_data[0:SAMPLES_PER_MONTH], 'fixing_i_volume', title='Dane dla miesiąca')
-# Dane dla roku
-plot_time_series(electricity_data[0:SAMPLES_PER_YEAR], 'fixing_i_volume', title='Dane dla roku')
+# # Dane dla jednego dnia 
+# plot_time_series(electricity_data[0:SAMPLES_PER_DAY], 'fixing_i_volume', title='Dane dla jednego dnia')
+# # Dane dla tygodnia 
+# plot_time_series(electricity_data[0:SAMPLES_PER_WEEK], 'fixing_i_volume', title='Dane dla tygodnia')
+# # Dane dla miesiąca
+# plot_time_series(electricity_data[0:SAMPLES_PER_MONTH], 'fixing_i_volume', title='Dane dla miesiąca')
+# # Dane dla roku
+# plot_time_series(electricity_data[0:SAMPLES_PER_YEAR], 'fixing_i_volume', title='Dane dla roku')
